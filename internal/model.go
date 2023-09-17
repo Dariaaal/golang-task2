ab5cec53542935ab94efff5365ed065efbc9b494
Dariaaal <111464521+Dariaaal@users.noreply.github.com>
2023-09-17 10:33:33 +0300

    model

--- internal/model.go
+++ internal/model.go
@@ -1,4 +1,14 @@
 package model
 
 type Product struct {
+	ID          int     `json:"uid"`
+	Cover       []Cover `json:"cover"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Price       int     `json:"price"`
+}
+
+type Cover struct {
+	Url  string `json:"url"`
+	Type string `json:"type"`
 }
